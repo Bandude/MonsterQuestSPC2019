ed620935f767a50dbbf3a1c64a4ee9b60d0dfcb8
Craig Jones <48070856+Bandude@users.noreply.github.com>
2020-03-06 15:42:10 -0600

    Create AddAttackChoice.py

---
+++ ClassWork/AddAttackChoice.py
@@ -0,0 +1,8 @@
+    while EnemyHealth > 0:
+        print("-----------------------")
+        choice = input("Choose What to Do: \n S - Attack with Sword \n Your Choice: ")
+        type(choice)
+        print(clear)
+      
+        if(choice == "S"):
+            EnemyHealth = attack(PlayerName, PlayerWeapon[0], PlayerAtkBonus, PlayerStrength, EnemyName, EnemyArmor, EnemyHealth, EnemyOrigHealth, True)
